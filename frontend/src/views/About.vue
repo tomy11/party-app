43f8c2e30531f260891161ba3193928340272390
DUORIAND\Duoriand <ohnemotive74@gmail.com>
2021-06-02 00:56:34 +0700

    update error

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,51 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="md:container md:mx-auto">
+      <h1>This is an about page</h1>
+      <p>
+        <a href="https://tailwindcss.com/docs" class="text-cyan-600 hover:text-cyan-700"> Read the docs &rarr; </a>
+      </p>
+    </div>
+    <div class="chat-notification">
+      <div class="chat-notification-logo-wrapper">
+          <img class="chat-notification-logo" src="" alt="ChitChat Logo">
+      </div>
+       <div class="chat-notification-content">
+          <h4 class="chat-notification-title">ChitChat</h4>
+          <p class="chat-notification-message">You have a new message!</p>
+       </div>
+    </div>
   </div>
 </template>
+<style>
+  .chat-notification {
+    display: flex;
+    max-width: 24rem;
+    margin: 0 auto;
+    padding: 1.5rem;
+    border-radius: 0.5rem;
+    background-color: #fff;
+    box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04);
+  }
+  .chat-notification-logo-wrapper {
+    flex-shrink: 0;
+  }
+  .chat-notification-logo {
+    height: 3rem;
+    width: 3rem;
+  }
+  .chat-notification-content {
+    margin-left: 1.5rem;
+    padding-top: 0.25rem;
+  }
+  .chat-notification-title {
+    color: #1a202c;
+    font-size: 1.25rem;
+    line-height: 1.25;
+  }
+  .chat-notification-message {
+    color: #718096;
+    font-size: 1rem;
+    line-height: 1.5;
+  }
+</style>
